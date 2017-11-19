5426d287745f51f5d41b50c5e5df72e839222991
javier <javieresos12@gmail.com>
2017-11-19 16:30:53 -0500

    proyecto base de web

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+  resources :rol_opcion_operaciones
+  resources :opciones
+  resources :valor_parametros
+  resources :usuarios
+  root 'seguridad#login'
+  resources :parametros
+  post '/auth', to:'seguridad#autenticar'
+  get '/inicio', to:'inicio#casa'
+  get '/logout', to:'seguridad#logout'
+  get '/valp/:id', to: "valor_parametros#index"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
